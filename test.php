53016769906de90d1adf43e99967d0020263a30a
Nicolas-Puchois <nicolas.puchois@laposte.net>
2025-03-12 08:54:24 +0100

    initial commit

---
+++ test.php
@@ -0,0 +1,44 @@
+<?php
+
+// $monTableau = [];
+// $N = readline("Veuillez saisir le degré : ");
+
+// for ($i = 1; $i <= $N; $i = $i + 1) {
+//     for ($j = 1; $j <= $i; $j++) {
+//         if ($j == 1) {
+//             $monTableau[$i][$j] = 1;
+//         } elseif ($j == $i) {
+//             $monTableau[$i][$j] = 1;
+//         } else {
+//             $monTableau[$i][$j] = $monTableau[$i - 1][$j - 1] + $monTableau[$i - 1][$j];
+//         }
+//         echo ($monTableau[$i][$j]);
+//     }
+//     echo ("\n");
+// }
+
+$nombre_minimal = readline("Veuillez saisir le nombre minimal pour la plage : ");
+$nombre_maximal = readline("Veuillez saisir le nombre maximal pour la plage : ");
+$nombre_aleatiore = rand($nombre_minimal, $nombre_maximal);
+$nombre_utilisateur = readline("Veuillez saisir un nombre: ");
+$compteur = 1;
+$nbre_essaie = 5;
+
+while ($nombre_utilisateur != $nombre_aleatiore && $nbre_essaie - 1  > 0) {
+    $compteur++;
+    $nbre_essaie--;
+    echo ("Il vous reste " . $nbre_essaie . " d'essais ");
+    if ($nombre_utilisateur < $nombre_aleatiore) {
+        echo ("Le nombre est plus grand \n");
+        $nombre_utilisateur = readline("Veuillez saisir un nombre: ");
+    } else if ($nombre_utilisateur > $nombre_aleatiore) {
+        echo ("Le nombre est plus petit \n");
+        $nombre_utilisateur = readline("Veuillez saisir un nombre: ");
+    }
+}
+if ($nombre_utilisateur == $nombre_aleatiore && $nbre_essaie > 0) {
+    echo ("bravo vous avez trouvé en " . $compteur . " d'essaie !! \n");
+    echo (" Il vous reste " . $nbre_essaie . " d'essaies. ");
+} else {
+    echo ("Dommage, vous n'avez pas trouver le nombre " . $nombre_aleatiore . " :( ");
+}
